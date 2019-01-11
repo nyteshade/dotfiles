df661c38cdab0a983dd3e3e09ab1ec362cb35330
Cody Winton <cody.t.winton@gmail.com>
2019-01-11 11:36:43 -0800

    Added Dock Prefs

--- macos/install.sh
+++ macos/install.sh
@@ -7,3 +7,11 @@ SCREENSHOTDIR="${HOME}/Pictures/Screenshots"
 test -d "${SCREENSHOTDIR}" || mkdir "${SCREENSHOTDIR}"
 
 defaults write com.apple.screencapture location $SCREENSHOTDIR
+
+# Dock Prefs
+
+defaults write com.apple.dock autohide -bool yes;
+defaults write com.apple.dock autohide-delay -float 0;
+defaults write com.apple.dock autohide-time-modifier -float 0.14999999999999999;
+
+killall Dock
